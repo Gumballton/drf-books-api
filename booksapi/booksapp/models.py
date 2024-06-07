2ba8b5b58f85b9a56f05d223be7f3d8017002847
Gumballton <pvova6620@gmail.com>
2024-06-07 13:20:12 +0300

    model for books

--- booksapi/booksapp/models.py
+++ booksapi/booksapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class BooksModel(models.Model):
+    author = models.CharField(max_length=1000)
+    book_name = models.CharField(max_length=3000)
+    book_link = models.CharField(max_length=3000)
+    book_image = models.BinaryField(null=True)
+
+    # If you have the table in db
+    class Meta:
+        # Provide the name of your existing table in the database
+        db_table = 'books_data'
+
+    def __str__(self):
+        return self.book_name
